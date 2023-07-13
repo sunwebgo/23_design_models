58b3496511a82fea6de2ef08a9b1d196a8486d21
xuhuaiang <xuhuaiang2000@163.com>
2023-07-13 22:28:05 +0800

    🔥add proxy model

---
+++ src/main/java/com/xha/model/construct/proxy/cglib_proxy/TrainStation.java
@@ -0,0 +1,13 @@
+package com.xha.model.construct.proxy.cglib_proxy;
+
+/**
+ * 火车站
+ *
+ * @author Xu huaiang
+ * @date 2023/07/13
+ */
+public class TrainStation {
+    public void sell(){
+        System.out.println("火车站卖票");
+    }
+}
---
+++ src/main/java/com/xha/model/construct/proxy/jdk_proxy/TestDemo.java
@@ -0,0 +1,18 @@
+package com.xha.model.construct.proxy.jdk_proxy;
+
+/**
+ * 测试演示
+ *
+ * @author Xu huaiang
+ * @date 2023/07/13
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+//        1.创建火车站对象
+        ProxyFactory proxyFactory = new ProxyFactory();
+//        2.调用代理工厂的方法获取代理对象
+        SellTickets proxyObject = proxyFactory.getProxyObject();
+//        3.调用代理对象的卖票方法
+        proxyObject.sell();
+    }
+}
