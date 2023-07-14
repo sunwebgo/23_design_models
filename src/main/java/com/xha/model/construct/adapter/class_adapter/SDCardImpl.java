7b842388952f165da73290c7347f2c7c98f6c3c6
xuhuaiang <xuhuaiang2000@163.com>
2023-07-14 09:21:33 +0800

    🔥add adapter model

---
+++ src/main/java/com/xha/model/construct/adapter/class_adapter/SDCardImpl.java
@@ -0,0 +1,20 @@
+package com.xha.model.construct.adapter.class_adapter;
+
+/**
+ * sdcard impl
+ *
+ * @author Xu huaiang
+ * @date 2023/07/13
+ */
+public class SDCardImpl implements SDCard{
+    @Override
+    public String readSD() {
+        String msg = "SDCard read msg : hello word SDCard";
+        return msg;
+    }
+
+    @Override
+    public void writeSD(String msg) {
+        System.out.println("SDCard write msg : " + msg);
+    }
+}
