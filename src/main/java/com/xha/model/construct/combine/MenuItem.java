af85ca240aac0850df7faf5b8001f4d577b2d0c4
xuhuaiang <xuhuaiang2000@163.com>
2023-07-14 16:45:04 +0800

    🔥add combine model

---
+++ src/main/java/com/xha/model/construct/combine/MenuItem.java
@@ -0,0 +1,23 @@
+package com.xha.model.construct.combine;
+
+/**
+ * 叶子节点
+ *
+ * @author Xu huaiang
+ * @date 2023/07/14
+ */
+public class MenuItem extends MenuComponent{
+
+    public MenuItem(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    @Override
+    public void print() {
+        for (int i = 0; i < level; i++) {
+            System.out.print("--");
+        }
+        System.out.println(name);
+    }
+}
