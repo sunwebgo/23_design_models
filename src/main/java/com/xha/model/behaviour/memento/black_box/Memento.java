8143999ee632d1c7b7d047b51f8e84180c369d3b
xuhuaiang <xuhuaiang2000@163.com>
2023-07-15 16:25:31 +0800

    🔥add memento model

---
+++ src/main/java/com/xha/model/behaviour/memento/black_box/Memento.java
@@ -0,0 +1,10 @@
+package com.xha.model.behaviour.memento.black_box;
+
+/**
+ * 备忘录接口，对外提供窄接口
+ *
+ * @author Xu huaiang
+ * @date 2023/07/15
+ */
+public interface Memento {
+}
---
+++ src/main/java/com/xha/model/behaviour/memento/white_box/RoleStateCaretaker.java
@@ -0,0 +1,20 @@
+package com.xha.model.behaviour.memento.white_box;
+
+/**
+ * 管理者
+ *
+ * @author Xu huaiang
+ * @date 2023/07/15
+ */
+public class RoleStateCaretaker {
+
+    private RoleStateMemento memento;
+
+    public RoleStateMemento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(RoleStateMemento memento) {
+        this.memento = memento;
+    }
+}
